713d0c2f5b296e66ae90964f9e87ab7aac579269
Erin Jahan Tufa <105866826+tufa395@users.noreply.github.com>
2022-05-24 22:39:52 +0600

    Add files via upload

---
+++ lab-2/Problem-3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int number1;
+
+	printf("Enter the number :");
+	scanf("%d",&number1);
+
+
+	if(number1%2==0){
+
+		printf("Even Number");
+
+	}
+	else
+	{
+			printf("Odd Number");
+	}
+
+	return 0;
+
+}
