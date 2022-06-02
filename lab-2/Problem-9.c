f2bc4b26a384cb03fe9cc3fbeb2b4cc2a67369f5
Erin Jahan Tufa <105866826+tufa395@users.noreply.github.com>
2022-06-02 12:33:50 +0600

    Update Problem-9.c

--- lab-2/Problem-9.c
+++ lab-2/Problem-9.c
@@ -12,7 +12,7 @@ int main()
 	}
 	else if(a>=97 && a<=122)
 	{
-	   printf("You entered a capital letter");
+	   printf("You entered a small letter");
 	}
 	else if((a>=0 && a<=47) || (a>=58 && a<=64) || (a>=91 && a<=96) || (a>=123 && a<=127) )
 	{
